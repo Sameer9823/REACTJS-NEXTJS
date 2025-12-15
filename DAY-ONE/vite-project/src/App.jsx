3e5d954d35d678b0ea915bbaf45e34cd42d6ca6c
Sameer Selokar <sameerselokar924@gmail.com>
2025-12-15 22:34:22 +0530

    feat: add navigation menu and button components, update dependencies
    - Added @radix-ui/react-navigation-menu and related components for navigation.
    - Introduced a Button component with variant support using class-variance-authority.
    - Updated App component to include NavigationMenu and Button.
    - Enhanced styling with Tailwind CSS and tw-animate-css.
    - Created utility functions for class name merging.
    - Configured TypeScript paths for better module resolution.
    - Updated Vite configuration to support aliasing for src directory.

--- DAY-ONE/vite-project/src/App.jsx
+++ DAY-ONE/vite-project/src/App.jsx
@@ -5,13 +5,33 @@ import "./App.css";
 import Card from "./component/Card";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
+import { Button } from "./components/ui/button.tsx"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "./components/ui/navigation-menu.tsx"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-     
+    <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
       <Hero/>
       <Header/>
 
@@ -21,6 +41,11 @@ function App() {
     <Card title="Santa" imgUrl="https://images.pexels.com/photos/35216105/pexels-photo-35216105.jpeg"/>
     <Card title="banta" imgUrl="https://images.pexels.com/photos/35216105/pexels-photo-35216105.jpeg"/>
       </div>
+    <div className="flex justify-center items-center">
+
+
+      <Button variant={"outline"}>click me</Button>
+    </div>
 
       
     </>
