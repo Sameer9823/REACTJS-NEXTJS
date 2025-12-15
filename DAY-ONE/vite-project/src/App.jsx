c879e333d3acec26fd23595a5ccc1f594069ef34
Sameer Selokar <sameerselokar924@gmail.com>
2025-12-15 15:00:27 +0530

    style: format code and improve layout in App component

--- DAY-ONE/vite-project/src/App.jsx
+++ DAY-ONE/vite-project/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-4xl text-red-500'>Learn react with sameer</h1>
+      <h1 className="text-4xl text-red-500 border-2 border-blue-400">
+        Learn react with sameer
+      </h1>
+
+      <div className="max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition-shadow ">
+        <img src="https://images.pexels.com/photos/6129284/pexels-photo-6129284.jpeg" alt="img" className="w-full h-56 object-cover"/>
+        <div className="p-4">
+          <h2 className="text-lg font-semibold text-gray-800">Christmas Tree</h2>
+          <p className="text-gray-600 mt-2 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam numquam voluptates suscipit recusandae quis. Dolores.</p>
+          <button className="mt-4 px-4">Buy it</button>
+        </div>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
