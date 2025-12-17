bcea38137d7138f12da45868fd621418766b3f2e
Sameer Selokar <sameerselokar924@gmail.com>
2025-12-17 23:37:32 +0530

    feat: integrate Tailwind CSS and refactor App component structure
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored App component to include a new Navigation component and AppContent for better structure.
    - Removed existing CSS styles and replaced them with Tailwind CSS imports.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- Advanced/src/App.jsx
+++ Advanced/src/App.jsx
@@ -1,21 +1,41 @@
 
-import './App.css'
 import BasicProps from './components/BasicProps'
 import ChildrenProps from './components/ChildrenProps'
 import ComplexProps from './components/ComplexProps'
 import RefProps from './components/RefProps'
 import ThemeToggler from './components/ThemeToggler'
 
+
+function Navigation() {
+  const isDark = true
+
+  const sections = [
+    {id: 'basic', label: 'Basic Props' , icons: '✨'},
+    {id: 'ref', label: 'ref Props' , icons: '🔖'},
+    {id: 'children', label: 'Children Props' , icons: '🔗'},
+    {id: 'complex', label: 'Complex Props' , icons: '⚙️'},
+    {id: 'theme', label: 'Theme Toggler' , icons: '🎨'},
+  ]
+
+  return (
+    <nav className={'sticky top-0 z-50 shadow-md'}>
+
+    </nav>
+  )}
+
+  function AppContent(){
+    return (
+      <div className={'min-h-screen bg-gray-800'}>
+        test
+      </div>
+    )
+  }
 function App() {
   
 
   return (
     <>
-      <BasicProps/>
-      <ChildrenProps/>
-      <ComplexProps/>
-      <RefProps/>
-      <ThemeToggler/>
+      <AppContent />;
     </>
   )
 }
