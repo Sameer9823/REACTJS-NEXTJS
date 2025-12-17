7fe736de6b339af39bd8b436acd2e929e712c15c
Sameer Selokar <sameerselokar924@gmail.com>
2025-12-17 17:10:42 +0530

    feat: initialize Advanced React project with Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main component, importing various feature components.
    - Added React logo SVG to assets.
    - Developed BasicProps, ChildrenProps, ComplexProps, RefProps, and ThemeToggler components.
    - Established global styles in index.css for consistent theming.
    - Set up main.jsx to render the App component using React's StrictMode.
    - Configured Vite with a basic vite.config.js file.

---
+++ Advanced/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import BasicProps from './components/BasicProps'
+import ChildrenProps from './components/ChildrenProps'
+import ComplexProps from './components/ComplexProps'
+import RefProps from './components/RefProps'
+import ThemeToggler from './components/ThemeToggler'
+
+function App() {
+  
+
+  return (
+    <>
+      <BasicProps/>
+      <ChildrenProps/>
+      <ComplexProps/>
+      <RefProps/>
+      <ThemeToggler/>
+    </>
+  )
+}
+
+export default App
